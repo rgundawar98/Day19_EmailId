ba5b14d6fbe2e2a36be1d3101a9cf500e5f1f192
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-03-02 20:34:56 +0530

    Adding welcome message

---
+++ Day19_CheckEmailId/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day19_CheckEmailId
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the regular expression Email Id");
+        }
+    }
+}
